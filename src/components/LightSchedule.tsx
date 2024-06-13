c5966152eb1ea988d47dcacc1e85bb426a5586cd
Atul Kumar <atul.tinymesh.in>
2024-06-13 14:36:49 +0530

    brightness bar changes

--- src/components/LightSchedule.tsx
+++ src/components/LightSchedule.tsx
@@ -7,8 +7,71 @@ import { LightScheduleType } from "containers/LightSchedule";
 import { SLCState } from "contexts/SLC";
 import dayjs from "dayjs";
 import customParseFormat from 'dayjs/plugin/customParseFormat';
+import Slider, { SliderThumb, SliderValueLabelProps } from '@mui/material/Slider';
+import { styled } from '@mui/material/styles';
+import Tooltip from '@mui/material/Tooltip';
 
 type RangePickerProps = GetProps<typeof DatePicker.RangePicker>;
+
+function ValueLabelComponent(props: SliderValueLabelProps) {
+  const { children, value } = props;
+
+  return (
+    <Tooltip enterTouchDelay={0} placement="top" title={value} >
+      {children}
+    </Tooltip>
+  );
+}
+
+const iOSBoxShadow =
+  '0 3px 1px rgba(0,0,0,0.1),0 4px 8px rgba(0,0,0,0.13),0 0 0 1px rgba(0,0,0,0.02)';
+
+  const IOSSlider = styled(Slider)(({ theme }) => ({
+    color: theme.palette.mode === 'dark' ? '#0a84ff' : '#007bff',
+    height: 5,
+    padding: '15px 0',
+    '& .MuiSlider-thumb': {
+      height: 20,
+      width: 20,
+      backgroundColor: '#050C9C',
+      boxShadow: '0 0 2px 0px rgba(0, 0, 0, 0.1)',
+      '&:focus, &:hover, &.Mui-active': {
+        boxShadow: '0px 0px 3px 1px #000',
+        // Reset on touch devices, it doesn't add specificity
+        '@media (hover: none)': {
+          boxShadow: iOSBoxShadow,
+        },
+      },
+      '&:before': {
+        boxShadow:
+          '0px 0px 1px 0px rgba(0,0,0,0.2), 0px 0px 0px 0px rgba(0,0,0,0.14), 0px 0px 1px 0px rgba(0,0,0,0.12)',
+      },
+    },
+    '& .MuiSlider-valueLabel': {
+      fontSize: 12,
+      fontWeight: 'normal',
+      top: -6,
+      backgroundColor: 'unset',
+      color: theme.palette.text.primary,
+      '&::before': {
+        display: 'none',
+      },
+      '& *': {
+        background: 'transparent',
+        color: theme.palette.mode === 'dark' ? '#fff' : '#000',
+      },
+    },
+    '& .MuiSlider-track': {
+      border: 'none',
+      height: 5,
+      background: "#3572EF",
+    },
+    '& .MuiSlider-rail': {
+      opacity: 0.5,
+      boxShadow: 'inset 0px 0px 4px -2px #000',
+      backgroundColor: '#686D76',
+    },
+  }));  
 
 dayjs.extend(customParseFormat);
 
@@ -74,7 +137,7 @@ const LightSchedule: React.FC<LightScheduleType> = ({ slcState, lightMode, handl
       title: "Schedule",
       dataIndex: "schedule",
       key: "schedule",
-      width: 50,
+      width: 60,
       render: (_, record: SLCState) => (
         <RangePicker
           size="large"
@@ -98,26 +161,27 @@ const LightSchedule: React.FC<LightScheduleType> = ({ slcState, lightMode, handl
       title: "Brightness",
       dataIndex: "brightness",
       key: "brightness",
-      width: 18,
+      width: 25,
       render: (_, record: SLCState) => (
-        <Input
-          size="large"
-          type="number"
-          placeholder="default = 0"
-          min="0"
-          max="100"
-          value={record.brightness}
-          suffix={<PercentageOutlined />}
-          style={{ borderColor: lightMode? "silver" : "gray" }}
-          onChange={(e) => handleBrightnessChange(record.key, parseInt(e.target.value, 10))} 
-        />
+        // <Input
+        //   size="large"
+        //   type="number"
+        //   placeholder="default = 0"
+        //   min="0"
+        //   max="100"
+        //   value={record.brightness}
+        //   suffix={<PercentageOutlined />}
+        //   style={{ borderColor: lightMode? "silver" : "gray" }}
+        //   onChange={(e) => handleBrightnessChange(record.key, parseInt(e.target.value, 10))} 
+        // />
+        <IOSSlider aria-label="ios slider" defaultValue={60} valueLabelDisplay="on" />
       ),
     },
     {
       title: <button className="btn btn-active btn-error" onClick={handleScheduleEvent}>Schedule</button>,
       dataIndex: "scheduler_info",
       key: "scheduler_info",
-      width: 30,
+      width: 27,
       // && record.scheduler!.length !== 0
       // format('DD/MM/YYYY HH:MM Z') to add timezone
       render: (_, record: SLCState) => record !== null && record !== undefined && record.scheduler ? <>
